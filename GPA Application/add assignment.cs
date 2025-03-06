f4e4bc175efdcfaebe3d0127c82cec619345020d
Dewmi64 <ar112689@fhss.sjp.ac.lk>
2025-03-07 01:42:58 +0530

    Implement Add New Assignment feature with form input and storage

--- GPA Application/add assignment.cs
+++ GPA Application/add assignment.cs
@@ -12,14 +12,22 @@ namespace GPA_Application
 {
     public partial class add_assignment : Form
     {
+        private Assignment_Storage Assignment_Storage;
         public add_assignment()
         {
             InitializeComponent();
+            Assignment_Storage = new Assignment_Storage();
         }
 
         private void add_assignment_Load(object sender, EventArgs e)
         {
+            string subjectName = richTextBox1.Text.Trim();
+            string description = richTextBox2.Text.Trim();
+            DateTime dueDate = dateTimePicker1.Value;
 
+            Assignment_Details newAssignment = new Assignment_Details(subjectName, description, dueDate);
+            Assignment_Storage.AddAssignment(newAssignment); // Add assignment to the list
         }
     }
 }
+
