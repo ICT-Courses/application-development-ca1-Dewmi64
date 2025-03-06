7ef21911bcb100c6ba6f211c2326468deb96f646
Dewmi64 <ar112689@fhss.sjp.ac.lk>
2025-03-07 00:41:23 +0530

    Add AssignmentDetails class for handling assignment details

---
+++ GPA Application/Assignment Details.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GPA_Application
+{
+    public class Assignment_Details
+    {
+        public string SubjectName { get; set; }
+        public string Description { get; set; }
+        public DateTime DueDate { get; set; }
+
+        public Assignment_Details(string subjectName, string description, DateTime dueDate)
+        {
+            SubjectName = subjectName;
+            Description = description;
+            DueDate = dueDate;
+        }
+    }
+}
