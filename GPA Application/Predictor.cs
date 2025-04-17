c554133d7c543a15317f85d6a8fe0e906324e41d
Dewmi64 <ar112689@fhss.sjp.ac.lk>
2025-04-17 07:14:34 -0600

    Implement Excell file import and display it in datagridview for GPA Predictor

---
+++ GPA Application/Predictor.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using ExcelDataReader;
+using MySql.Data.MySqlClient;
+
+namespace GPA_Application
+{
+    public partial class Predictor : Form
+    {
+        public Predictor()
+        {
+            InitializeComponent();
+        }
+
+
+        private DataTable excelTable;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog ofd = new OpenFileDialog() { Filter = "Excel Files|*.xls;*.xlsx" })
+            {
+                if (ofd.ShowDialog() == DialogResult.OK)
+                {
+                    using (var stream = File.Open(ofd.FileName, FileMode.Open, FileAccess.Read))
+                    {
+                        using (var reader = ExcelReaderFactory.CreateReader(stream))
+                        {
+                            var conf = new ExcelDataSetConfiguration
+                            {
+                                ConfigureDataTable = _ => new ExcelDataTableConfiguration { UseHeaderRow = true }
+                            };
+
+                            var dataSet = reader.AsDataSet(conf);
+                            excelTable = dataSet.Tables[0];
+                            dataGridView1.DataSource = excelTable;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
