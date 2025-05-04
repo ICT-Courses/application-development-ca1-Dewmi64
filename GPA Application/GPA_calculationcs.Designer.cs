98ff8a40ea344e84124c275228b47e9c74ea833c
Dewmi64 <ar112689@fhss.sjp.ac.lk>
2025-05-04 16:20:52 +0530

    Implement GPA Calculation for calculate semseter GPA and Overall GPA

---
+++ GPA Application/GPA_calculationcs.Designer.cs
@@ -0,0 +1,243 @@
+﻿namespace GPA_Application
+{
+    partial class GPA_calculationcs
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(GPA_calculationcs));
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("MS Reference Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.button1.Location = new System.Drawing.Point(271, 119);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(180, 45);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Upload File";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Font = new System.Drawing.Font("MS Reference Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "1st Year 1st Semester",
+            "1st Year 2nd Semester",
+            "2nd Year 1st Semester",
+            "2nd Year 1st Semester",
+            "3rd Year 1st Semester",
+            "3rd Year 2nd Semester",
+            "4th Year 1st Semester",
+            "4th Year 2nd Semester"});
+            this.comboBox1.Location = new System.Drawing.Point(271, 227);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(180, 31);
+            this.comboBox1.TabIndex = 1;
+            this.comboBox1.Text = "Select Semester";
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("MS Reference Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.button2.Location = new System.Drawing.Point(39, 291);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(412, 46);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Calculate Your GPA";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Font = new System.Drawing.Font("MS Reference Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBox1.Location = new System.Drawing.Point(195, 388);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(256, 32);
+            this.richTextBox1.TabIndex = 3;
+            this.richTextBox1.Text = "";
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("MS Reference Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.button3.Location = new System.Drawing.Point(360, 676);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(91, 49);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Save";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("MS Reference Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.button4.Location = new System.Drawing.Point(39, 539);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(412, 43);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "Calculate Overall GPA";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("MS Reference Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button5.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.button5.Location = new System.Drawing.Point(39, 459);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(416, 42);
+            this.button5.TabIndex = 6;
+            this.button5.Text = "Save";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Font = new System.Drawing.Font("MS Reference Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBox2.Location = new System.Drawing.Point(39, 607);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(412, 40);
+            this.richTextBox2.TabIndex = 7;
+            this.richTextBox2.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("MS Reference Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Aqua;
+            this.label1.Location = new System.Drawing.Point(35, 126);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(204, 23);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Upload your results";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("MS Reference Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Aqua;
+            this.label2.Location = new System.Drawing.Point(40, 227);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(170, 23);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Select semester";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Transparent;
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.richTextBox1);
+            this.panel1.Controls.Add(this.button3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.comboBox1);
+            this.panel1.Controls.Add(this.richTextBox2);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.button5);
+            this.panel1.Controls.Add(this.button4);
+            this.panel1.Location = new System.Drawing.Point(523, 53);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(508, 748);
+            this.panel1.TabIndex = 10;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("MS Reference Sans Serif", 24F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.Aqua;
+            this.label3.Location = new System.Drawing.Point(85, 20);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(322, 49);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "GPA Caculator";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("MS Reference Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.Aqua;
+            this.label4.Location = new System.Drawing.Point(39, 394);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(102, 23);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Your GPA";
+            // 
+            // GPA_calculationcs
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1067, 813);
+            this.Controls.Add(this.panel1);
+            this.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.Name = "GPA_calculationcs";
+            this.Text = "GPA_calculationcs";
+            this.Load += new System.EventHandler(this.GPA_calculationcs_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
---
+++ GPA Application/GPA_calculationcs.cs
@@ -0,0 +1,278 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using ExcelDataReader;
+using MySql.Data.MySqlClient;
+
+
+
+namespace GPA_Application
+{
+    public partial class GPA_calculationcs : Form
+    {
+        public GPA_calculationcs()
+        {
+            InitializeComponent();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (excelTable == null || excelTable.Rows.Count == 0)
+            {
+                MessageBox.Show("No data to save.");
+                return;
+            }
+
+            string selectedSemester = comboBox1.SelectedItem?.ToString();
+            if (string.IsNullOrEmpty(selectedSemester))
+            {
+                MessageBox.Show("Please select a semester.");
+                return;
+            }
+
+            string connectionString = "server=localhost;user=root;database=grading_scale_database;password=Hwcs@1969;";
+            double totalCredits = 0;
+
+            foreach (DataRow row in excelTable.Rows)
+            {
+                totalCredits += Convert.ToDouble(row["No of Credits"]);
+            }
+
+            double semesterGPA = 0;
+            if (richTextBox1.Text.Contains(":"))
+            {
+                string[] parts = richTextBox1.Text.Split(':');
+                if (parts.Length > 1)
+                {
+                    double.TryParse(parts[1], out semesterGPA);
+                }
+            }
+
+            using (MySqlConnection con = new MySqlConnection(connectionString))
+            {
+                con.Open();
+
+                // Total Earned Grade Points tracker
+                double totalEarnedGradePoints = 0;
+
+                foreach (DataRow row in excelTable.Rows)
+                {
+                    string subjectName = row["Subject"].ToString();
+                    string grade = row["Grade"].ToString();
+                    double credits = Convert.ToDouble(row["No of Credits"]);
+
+                    string gpvQuery = "SELECT GPV FROM Grading_scale WHERE Grade = @grade";
+                    MySqlCommand gpvCmd = new MySqlCommand(gpvQuery, con);
+                    gpvCmd.Parameters.AddWithValue("@grade", grade);
+                    object gpvResult = gpvCmd.ExecuteScalar();
+                    double gpv = gpvResult != null ? Convert.ToDouble(gpvResult) : 0;
+
+                    double earnedGradePoints = gpv * credits;
+                    totalEarnedGradePoints += earnedGradePoints;
+
+                    string insertQuery = @"INSERT INTO SemesterResults
+                (SemesterName, SubjectName, Grade, TotalNumberOfCredits, GPA, TotalEarnedGradePoints)
+                 VALUES
+                (@semester, @subject, @grade, @credits, @gpa, @earnedGP)";
+
+                    using (MySqlCommand cmd = new MySqlCommand(insertQuery, con))
+                    {
+                        cmd.Parameters.AddWithValue("@semester", selectedSemester);
+                        cmd.Parameters.AddWithValue("@subject", subjectName);
+                        cmd.Parameters.AddWithValue("@grade", grade);
+                        cmd.Parameters.AddWithValue("@credits", credits);
+                        cmd.Parameters.AddWithValue("@gpa", semesterGPA);
+                        cmd.Parameters.AddWithValue("@earnedGP", earnedGradePoints);
+                        cmd.ExecuteNonQuery();
+                    }
+                }
+
+                // ✅ Insert cumulative summary logic here
+                double cumulativeCredits = 0;
+                double cumulativeEarnedGP = 0;
+
+                // Step 1: Get sum of all previous semesters
+                string sumQuery = @"SELECT 
+                                IFNULL(SUM(TotalNumberOfCredits), 0), 
+                                IFNULL(SUM(TotalEarnedGradePoints), 0) 
+                            FROM SemesterResults 
+                            WHERE SemesterName != @semester";
+
+                using (MySqlCommand sumCmd = new MySqlCommand(sumQuery, con))
+                {
+                    sumCmd.Parameters.AddWithValue("@semester", selectedSemester);
+                    using (MySqlDataReader reader = sumCmd.ExecuteReader())
+                    {
+                        if (reader.Read())
+                        {
+                            cumulativeCredits = Convert.ToDouble(reader[0]) + totalCredits;
+                            cumulativeEarnedGP = Convert.ToDouble(reader[1]) + totalEarnedGradePoints;
+                        }
+                    }
+                }
+
+                // Calculate Overall GPA
+                double overallGPA = cumulativeCredits > 0 ? cumulativeEarnedGP / cumulativeCredits : 0;
+
+                // Insert or Update to cumulative table
+                string insertCumulativeQuery = @"INSERT INTO CumulativeSemesterResults 
+            (SemesterName, TotalCreditsUpToThisSemester, TotalEarnedGradePointsUpToThisSemester, OverallGPA) 
+            VALUES (@semester, @totalCredits, @totalEarnedGP, @overallGPA)
+            ON DUPLICATE KEY UPDATE 
+                TotalCreditsUpToThisSemester = @totalCredits,
+                TotalEarnedGradePointsUpToThisSemester = @totalEarnedGP,
+                OverallGPA = @overallGPA";
+
+                using (MySqlCommand cmd = new MySqlCommand(insertCumulativeQuery, con))
+                {
+                    cmd.Parameters.AddWithValue("@semester", selectedSemester);
+                    cmd.Parameters.AddWithValue("@totalCredits", cumulativeCredits);
+                    cmd.Parameters.AddWithValue("@totalEarnedGP", cumulativeEarnedGP);
+                    cmd.Parameters.AddWithValue("@overallGPA", overallGPA);
+                    cmd.ExecuteNonQuery();
+                }
+
+                con.Close();
+            }
+
+            MessageBox.Show("Data saved successfully, including cumulative GPA!");
+        
+        }
+            
+        private DataTable excelTable;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            {
+                using (OpenFileDialog ofd = new OpenFileDialog() { Filter = "Excel Files|*.xls;*.xlsx" })
+                {
+                    if (ofd.ShowDialog() == DialogResult.OK)
+                    {
+                        using (var stream = File.Open(ofd.FileName, FileMode.Open, FileAccess.Read))
+                        {
+                            using (var reader = ExcelReaderFactory.CreateReader(stream))
+                            {
+                                var conf = new ExcelDataSetConfiguration
+                                {
+                                    ConfigureDataTable = _ => new ExcelDataTableConfiguration { UseHeaderRow = true }
+                                };
+
+                                var dataSet = reader.AsDataSet(conf);
+                                excelTable = dataSet.Tables[0];
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+            
+
+        private void GPA_calculationcs_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string selectedSemester = comboBox1.SelectedItem?.ToString();
+
+            if (string.IsNullOrEmpty(selectedSemester))
+            {
+                MessageBox.Show("Please select a semester.");
+                return;
+            }
+
+            double totalGradePoints = 0;  // Total grade points for current semester
+            double totalCredits = 0;       // Total credits for current semester
+
+            string connectionString = "server=localhost;user=root;database=grading_scale_database;password=Hwcs@1969;";
+
+            using (MySqlConnection con = new MySqlConnection(connectionString))
+            {
+                con.Open();
+
+                foreach (DataRow row in excelTable.Rows)
+                {
+                    string grade = row["Grade"].ToString();
+                    double credits = Convert.ToDouble(row["No of Credits"]);
+
+                    string query = "SELECT GPV FROM Grading_scale WHERE Grade = @grade";
+                    MySqlCommand cmd = new MySqlCommand(query, con);
+                    cmd.Parameters.AddWithValue("@grade", grade);
+
+                    object result = cmd.ExecuteScalar();
+
+                    if (result != null)
+                    {
+                        double gpv = Convert.ToDouble(result);
+                        double earnedGradePoints = gpv * credits;
+
+                        totalGradePoints += earnedGradePoints;
+                        totalCredits += credits;
+                    }
+                }
+
+                con.Close();
+            }
+
+            // Calculate GPA for the selected semester
+            double semesterGPA = totalCredits > 0 ? totalGradePoints / totalCredits : 0;
+            richTextBox1.Text = "  Your GPA IS: " + semesterGPA.ToString("0.00");
+
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+      
+        {
+            string selectedSemester = comboBox1.SelectedItem?.ToString();
+
+            if (string.IsNullOrEmpty(selectedSemester))
+            {
+                MessageBox.Show("Please select a semester.");
+                return;
+            }
+
+            string connectionString = "server=localhost;user=root;database=grading_scale_database;password=Hwcs@1969;";
+            double overallGPA = 0;
+
+            using (MySqlConnection con = new MySqlConnection(connectionString))
+            {
+                con.Open();
+
+                string query = @"SELECT OverallGPA 
+                         FROM CumulativeSemesterResults 
+                         WHERE SemesterName = @semester";
+
+                using (MySqlCommand cmd = new MySqlCommand(query, con))
+                {
+                    cmd.Parameters.AddWithValue("@semester", selectedSemester);
+                    object result = cmd.ExecuteScalar();
+
+                    if (result != null)
+                    {
+                        overallGPA = Convert.ToDouble(result);
+                        richTextBox2.Text = "  Your Overall GPA IS: " + overallGPA.ToString("0.00");
+                    }
+                    else
+                    {
+                        richTextBox2.Text = "  No GPA data found for the selected semester.";
+                    }
+                }
+
+                con.Close();
+            }
+        }
+
+    }
+}
+
+
