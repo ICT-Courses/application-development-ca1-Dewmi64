28dd56508466369a876c5519e03b8f78f9bf6c8d
Dewmi64 <ar112689@fhss.sjp.ac.lk>
2025-05-04 22:38:39 +0530

    Implemnt weak area tracker including richtextbox,listbox and buttons

---
+++ GPA Application/Weak_Area_Tracker.Designer.cs
@@ -0,0 +1,189 @@
+﻿namespace GPA_Application
+{
+    partial class Weak_Area_Tracker
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Weak_Area_Tracker));
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label4 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(56, 233);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(341, 148);
+            this.listBox1.TabIndex = 0;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.richTextBox1.Location = new System.Drawing.Point(56, 449);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(341, 190);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.button1.Location = new System.Drawing.Point(56, 106);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(341, 52);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Analyze Weak Area";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.comboBox1.Font = new System.Drawing.Font("MS Reference Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "1st Year 1st Semester",
+            "1st Year 2nd Semester",
+            "2nd Year 1st Semester",
+            "2nd Year 1st Semester",
+            "3rd Year 1st Semester",
+            "3rd Year 2nd Semester",
+            "4th Year 1st Semester",
+            "4th Year 2nd Semester"});
+            this.comboBox1.Location = new System.Drawing.Point(56, 36);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(341, 31);
+            this.comboBox1.TabIndex = 14;
+            this.comboBox1.Text = "Select Semester";
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.label1.Location = new System.Drawing.Point(62, 196);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(121, 25);
+            this.label1.TabIndex = 15;
+            this.label1.Text = "Weak Areas";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(511, 361);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 16);
+            this.label2.TabIndex = 16;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.label3.Location = new System.Drawing.Point(62, 411);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(78, 25);
+            this.label3.TabIndex = 17;
+            this.label3.Text = "Adviser";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.listBox1);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.richTextBox1);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.comboBox1);
+            this.panel1.Location = new System.Drawing.Point(544, 22);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(461, 677);
+            this.panel1.TabIndex = 18;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("MS Reference Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label4.Location = new System.Drawing.Point(109, 174);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(355, 42);
+            this.label4.TabIndex = 19;
+            this.label4.Text = "Weak  Area Tracker";
+            // 
+            // Weak_Area_Tracker
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1067, 738);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label2);
+            this.Name = "Weak_Area_Tracker";
+            this.Text = "Weak_Area_Tracker";
+            this.Load += new System.EventHandler(this.Weak_Area_Tracker_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label4;
+    }
+}
---
+++ GPA Application/Weak_Area_Tracker.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace GPA_Application
+{
+    public partial class Weak_Area_Tracker : Form
+    {
+        public Weak_Area_Tracker()
+        {
+            InitializeComponent();
+        }
+
+        private void Weak_Area_Tracker_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string selectedSemester = comboBox1.SelectedItem?.ToString();
+            if (string.IsNullOrEmpty(selectedSemester))
+            {
+                MessageBox.Show("Please select a semester.");
+                return;
+            }
+
+            string connectionString = "server=localhost;user=root;database=grading_scale_database;password=Hwcs@1969;";
+            List<string> weakSubjects = new List<string>();
+            int totalSubjects = 0;
+
+            using (MySqlConnection con = new MySqlConnection(connectionString))
+            {
+                con.Open();
+                string query = @"SELECT SubjectName, Grade, TotalNumberOfCredits, GPA
+                               FROM SemesterResults
+                               WHERE SemesterName = @semester";
+                MySqlCommand cmd = new MySqlCommand(query, con);
+                cmd.Parameters.AddWithValue("@semester", selectedSemester);
+
+                using (MySqlDataReader reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        totalSubjects++;
+                        string subject = reader.GetString("SubjectName");
+                        string grade = reader.GetString("Grade");
+                        double gpv = Convert.ToDouble(reader["GPA"]);
+
+                        // Combined condition to identify weak areas
+                        if (gpv < 2.0 || grade == "C+" || grade == "C" || grade == "C-" || grade == "D" || grade == "E")
+                        {
+                            weakSubjects.Add($"{subject} (Grade: {grade})");
+                        }
+                    }
+                }
+            }
+
+            listBox1.Items.Clear();
+
+            if (totalSubjects == 0)
+            {
+                richTextBox1.Text = "📌 No data available for the selected semester.\n\nPlease try again after entering subject data. 📂";
+            }
+            else if (weakSubjects.Count == 0)
+            {
+                richTextBox1.Text = "You have no weak subjects! 👍\n\nKeep up the great work and continue studying consistently! 🧠📚";
+            }
+            else
+            {
+                foreach (string subject in weakSubjects)
+                {
+                    listBox1.Items.Add(subject);
+                }
+
+                richTextBox1.Text = "Focus more on the following subjects:\n\n" +
+                                    string.Join("\n", weakSubjects) +
+                                    "\n\n✅ Dedicate 3–4 hours per week to review!\n📌 Take notes, do revisions, and stay consistent.";
+            }
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            // Clear previous results
+            listBox1.Items.Clear();
+            richTextBox1.Clear();
+
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            panel1.BackColor = Color.FromArgb (100,0,0,0);
+        }
+    }
+}
+
