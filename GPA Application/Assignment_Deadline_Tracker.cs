52241fa53433a43db5d3668910bbbdd54960f97d
Dewmi64 <ar112689@fhss.sjp.ac.lk>
2025-05-04 22:41:41 +0530

    Create Assignment and Deadline tracker UI

---
+++ GPA Application/Assignment_Deadline_Tracker.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GPA_Application
+{
+    public partial class Assignment_Deadline_Tracker : Form
+    {
+        public Assignment_Deadline_Tracker()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            add_assignment f2 = new add_assignment();
+            this.Hide();
+            f2.Show();
+        }
+    }
+}
