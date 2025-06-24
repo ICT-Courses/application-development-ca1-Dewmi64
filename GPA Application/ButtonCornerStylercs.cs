4d0699ed1e18cf6ef00ca66a5aa1721b4d8fae9f
Dewmi64 <ar112689@fhss.sjp.ac.lk>
2025-06-23 21:41:03 -0600

    Implement button corner styler for stylish buttons

---
+++ GPA Application/ButtonCornerStylercs.cs
@@ -0,0 +1,21 @@
+﻿using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Windows.Forms;
+
+namespace GPA_Application
+{
+    public static class ButtonCornerStyler
+    {
+        public static void ApplyRoundedCorners(Button btn, int radius)
+        {
+            GraphicsPath path = new GraphicsPath();
+            path.AddArc(0, 0, radius, radius, 180, 90);
+            path.AddArc(btn.Width - radius, 0, radius, radius, 270, 90);
+            path.AddArc(btn.Width - radius, btn.Height - radius, radius, radius, 0, 90);
+            path.AddArc(0, btn.Height - radius, radius, radius, 90, 90);
+            path.CloseAllFigures();
+
+            btn.Region = new Region(path);
+        }
+    }
+}
